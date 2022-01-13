193f9bc841af6e469ac71907ecfcaffaa0c0739e
segster <bigbadseg@aol.com>
2022-01-13 13:08:52 +0000

    tree.py

--- tree.py
+++ tree.py
@@ -4,7 +4,7 @@ thr = "  00000 "
 fou = "   000 "
 fiv = "    0    "
 str = "    X"
+zero = "00000000000"
 
 
-
-print (f"{str}\n{fiv}\n{fou}\n{thr}\n{two}\n{one}")
+print (f"{str}\n{fiv}\n{fou}\n{thr}\n{two}\n{one}\n{zero}")
