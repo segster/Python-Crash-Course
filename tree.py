2c00e33f981190b1b4f9dd68e3092c0e24974df6
segster <bigbadseg@aol.com>
2022-01-14 08:11:04 +0000

    tree.py

--- tree.py
+++ tree.py
@@ -1,10 +1,13 @@
+#This will print a Christmas tree
+#Using variables
+
 one = "000000000"
 two = " 0000000"
 thr = "  00000 "
 fou = "   000 "
 fiv = "    0    "
 str = "    X"
-zero = "00000000000"
+zero = "0000000000"
 
 
 print (f"{str}\n{fiv}\n{fou}\n{thr}\n{two}\n{one}\n{zero}")
